e487fb53615556c1905e7332dccb936d5673183a
Bilal Shabbir <102783392+IBilalShabbir@users.noreply.github.com>
2022-09-10 14:29:51 +0500

    animation homepage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.scss";
 import Header from "./components/Header";
 import Home from "./screen/Home";
-import "custom-cursor-react/dist/index.css";
 
 function App() {
   return (
--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,53 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import bg3 from "../video/bg3.mp4";
-import CustomCursor from "custom-cursor-react";
+import { Fade } from "react-reveal";
 
 export default function Home() {
+  const [isBackToTop, setIsBackToTop] = useState(false);
+  useEffect(() => {
+    if (window.scrollY < 500) {
+      setIsBackToTop(true);
+    } else {
+      setIsBackToTop(false);
+    }
+    window.addEventListener("scroll", () => {
+      if (window.scrollY < 500) {
+        setIsBackToTop(true);
+      } else {
+        setIsBackToTop(false);
+      }
+    });
+  }, [window.scrollY]);
   return (
     <div className="homepage__container">
       <div className="homepage__container__wrapper__background">
         <div className="homepage__container__wrapper">
-          <div className="homepage__container__wrapper__entry">
-            <div className="homepage__container__wrapper__entry__content">
-              Di
+          <Fade spy={isBackToTop} appear left distance="10%">
+            <div className="homepage__container__wrapper__entry">
+              <div className="homepage__container__wrapper__entry__content">
+                Di
+              </div>
+              <div className="homepage__container__wrapper__entry__content__middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-edit-2"
+                >
+                  <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+                </svg>
+              </div>
+              <div className="homepage__container__wrapper__entry__content">
+                ital
+              </div>
             </div>
-            <div className="homepage__container__wrapper__entry__content__middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="1"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-edit-2"
-              >
-                <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-              </svg>
+          </Fade>
+          <Fade spy={isBackToTop} appear right distance="10%">
+            <div className="homepage__container__wrapper__entry__second">
+              <div className="homepage__container__wrapper__entry__content">
+                DESIGN
+              </div>
+              <div className="homepage__container__wrapper__entry__content__end">
+                <span>
+                  We build engaging user experience for early-stage startups by
+                  connecting the dots between users’ needs and the client’s
+                  business model.
+                </span>
+              </div>
             </div>
-            <div className="homepage__container__wrapper__entry__content">
-              ital
+          </Fade>
+          <Fade spy={isBackToTop} appear left distance="10%">
+            <div className="homepage__container__wrapper__entry">
+              <div className="homepage__container__wrapper__entry__content">
+                company
+              </div>
             </div>
-          </div>
-          <div className="homepage__container__wrapper__entry__second">
-            <div className="homepage__container__wrapper__entry__content">
-              DESIGN
-            </div>
-            <div className="homepage__container__wrapper__entry__content__end">
-              <span>
-                We build engaging user experience for early-stage startups by
-                connecting the dots between users’ needs and the client’s
-                business model.
-              </span>
-            </div>
-          </div>
-          <div className="homepage__container__wrapper__entry">
-            <div className="homepage__container__wrapper__entry__content">
-              company
-            </div>
-          </div>
+          </Fade>
         </div>
       </div>
       <div className="homepage__start__main">
@@ -55,18 +76,6 @@ export default function Home() {
           <video autoPlay loop muted width="100%">
             <source src={bg3} type="video/mp4" className="video"></source>
           </video>
-          <CustomCursor
-            targets={[".homepage__start__main__vid"]}
-            customClass="custom-cursor"
-            dimensions={1}
-            smoothness={{
-              movement: 0.2,
-              scale: 0.3,
-              opacity: 1,
-            }}
-            targetOpacity={1}
-            targetScale={150}
-          />
         </div>
       </div>
       <div className="more__about__us__section">
